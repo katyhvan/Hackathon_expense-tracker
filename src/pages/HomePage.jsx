75ed285ff503dbd7946f78b94c85809c17732e9c
Katerina <katyhvan@gmail.com>
2022-12-08 18:15:26 +0600

    added responsive homepae

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import two from '../img/two.png'
+import mobile from '../img/mobile_home.svg'
 import '../styles/HomePage.css'
 import Logo from './Logo'
 
@@ -8,14 +10,10 @@ const HomePage = () => {
 
   return (
     <>
-      {/* <img
-        style={{ position: "absolute", bottom: 0, right: 0 }}
-        className="mobile_home"
-        src={mobile}
-        alt=""
-      /> */}
+      <Logo />
+      <img src={mobile} className="home_img_mobile" />
       <div className="containerHome">
-        <Logo />
+        <img src={two} className="home_img" />
         <div className="block_right">
           <h2>Take your finances to the next levels!</h2>
           <h4>Don't hesite, money matters.</h4>
